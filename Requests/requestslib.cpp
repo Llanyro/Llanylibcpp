051e067a39c39c39913efbb882b7065cf76fddf9
Llanyro <Llanyro@gmail.com>
2021-06-09 10:41:52 +0200

    Starting Requests lib

---
+++ Requests/requestslib.cpp
@@ -0,0 +1,25 @@
+/*
+ * requestslib.cpp
+ *
+ *  Created on: Jun 9, 2021
+ *      Author: llanyro
+ */
+
+#include "requestslib.h"
+
+namespace Llanylib {
+namespace Requests {
+namespace Libs {
+
+requestslib::requestslib() {
+	// TODO Auto-generated constructor stub
+
+}
+
+requestslib::~requestslib() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace Libs */
+} /* namespace Requests */
+} /* namespace Llanylib */
---
+++ Requests/requestslib.h
@@ -0,0 +1,23 @@
+/*
+ * requestslib.h
+ *
+ *  Created on: Jun 9, 2021
+ *      Author: llanyro
+ */
+
+#ifndef REQUESTS_REQUESTSLIB_H_
+#define REQUESTS_REQUESTSLIB_H_
+
+#define REQUESTS_LIB Llanylib::Requests::Libs
+
+namespace Llanylib {
+namespace Requests {
+namespace Libs {
+
+void get();
+
+} /* namespace Libs */
+} /* namespace Requests */
+} /* namespace Llanylib */
+
+#endif /* REQUESTS_REQUESTSLIB_H_ */
